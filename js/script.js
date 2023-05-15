f6ca0cb58106b6e55b19c885dd83dfa5ccc18ba5
KamilObrzut <127477561+KamilObrzut@users.noreply.github.com>
2023-05-15 12:07:49 +0200

    Added Modals
    Added Modals and fixed some little issues

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@ const productInput = document.querySelector(".search");
 const products = document.querySelectorAll("li");
 const btnAddProduct = document.querySelectorAll(".btn-add-product");
 const userList = document.querySelector(".user-list");
-const btnAddProductSingle = document.querySelector(".btn-add-product");
 const btnCopyClipboard = document.querySelector(".copy-clipboard");
+const btnSort = document.querySelector(".sort-items");
 const btnClear = document.querySelector(".clear");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+const btnCloseCopyModal = document.querySelector(".btn-close-copy-modal");
+const modalBlur = document.querySelector(".modal-blur");
+const modalStart = document.querySelector(".modal-start");
+const modalCopy = document.querySelector(".modal-copy");
 
 let addedProducts = {};
 
-// Clear products list
+// Sort func
+const sortItems = () => {
+  const sortArr = Array.from(userList.children);
+  const newSortArrWithGroups = [];
+  console.log(sortArr);
+};
+
+// Clear products list function
 const clearList = () => {
   while (userList.firstChild) {
     userList.removeChild(userList.firstChild);
@@ -16,7 +28,7 @@ const clearList = () => {
   addedProducts = {};
 };
 
-// Copy list to clipboard
+// Copy list to clipboard function
 const copyToClipboard = () => {
   const arr = Array.from(userList.children);
   const copiedItems = [];
@@ -24,12 +36,19 @@ const copyToClipboard = () => {
     const slicedItem = item.textContent.slice(0, -1);
     copiedItems.push(slicedItem);
   });
-
   const copyText = copiedItems.join("\n");
   navigator.clipboard.writeText(copyText).then(() => {
-    alert("Skopiowano do schowka");
-    // Temporary alert - In future will be Modal
+    modalStart.classList.add("modal-hidden");
+    modalBlur.classList.add("modal-blur");
+    modalBlur.classList.remove("modal-hidden");
+    modalCopy.classList.add("modal-copy");
+    modalCopy.classList.remove("modal-hidden");
   });
+};
+
+// Close modals function
+const closeModal = () => {
+  modalBlur.classList.add("modal-hidden");
 };
 
 // Add/Remove elements from Users list function
@@ -67,7 +86,7 @@ for (let btn of btnAddProduct) {
   });
 }
 
-// Search
+// Search function
 const filterProducts = () => {
   products.forEach((item) => {
     const match = new RegExp(productInput.value, "i").test(item.textContent);
@@ -82,3 +101,6 @@ const filterProducts = () => {
 productInput.addEventListener("keyup", filterProducts);
 btnCopyClipboard.addEventListener("click", copyToClipboard);
 btnClear.addEventListener("click", clearList);
+btnCloseModal.addEventListener("click", closeModal);
+btnCloseCopyModal.addEventListener("click", closeModal);
+btnSort.addEventListener("click", sortItems);
